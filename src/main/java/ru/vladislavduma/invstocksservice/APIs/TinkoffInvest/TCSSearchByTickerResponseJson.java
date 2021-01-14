145653257c3cf4e39f3da0bbf943793d39fc8dd3
dumavla <vladislav.duma@experian.com>
2021-01-14 12:44:02 +0300

    Tinkoff SearchByTicker parsed

--- src/main/java/ru/vladislavduma/invstocksservice/APIs/TinkoffInvest/TCSSearchByTickerResponseJson.java
+++ src/main/java/ru/vladislavduma/invstocksservice/APIs/TinkoffInvest/TCSSearchByTickerResponseJson.java
@@ -1,6 +1,7 @@
 package ru.vladislavduma.invstocksservice.APIs.TinkoffInvest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,12 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TCSSearchByTickerResponseJson {
 
-//    @JsonProperty("instruments")
+    Integer total;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    String code;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     List<TCSSearchByTickerResponseInstrumentJson> instruments;
 
     public List<TCSSearchByTickerResponseInstrumentJson> getInstruments() {
@@ -20,6 +26,30 @@ public class TCSSearchByTickerResponseJson {
 
     public void setInstruments(List<TCSSearchByTickerResponseInstrumentJson> instruments) {
         this.instruments = instruments;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     @Override
